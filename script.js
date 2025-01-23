6480011a21b374bcbde00ddd98017239ffd006c7
Aksh4yyR <akshayraveendran559@gmail.com>
2025-01-23 12:20:13 +0530

    portfolioo

---
+++ script.js
@@ -0,0 +1,10 @@
+const typed=new Typed('.develop',{
+    strings:['MERN Full Stack Developer','MERN Full Stack Developer','MERN Full Stack Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+
